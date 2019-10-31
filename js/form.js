26376f157fb913ec95c47b30e09c1036ad9c58d0
Kassio Schaider <kassio.schaider@hotmail.com>
2019-10-31 15:46:36 -0300

    Adicionando o campo classificação

--- js/form.js
+++ js/form.js
@@ -7,6 +7,7 @@ for(var i = 0; i < pacientes.length ; i++){
     var tdPeso = paciente.querySelector(".info-peso");
     var tdAltura = paciente.querySelector(".info-altura");
     var tdImc = paciente.querySelector(".info-imc");
+    var tdClassificacao = paciente.querySelector(".info-classificacao");
 
     var peso = tdPeso.textContent;
     var altura = tdAltura.textContent;
@@ -29,8 +30,10 @@ for(var i = 0; i < pacientes.length ; i++){
     }
 
     if(pesoEhValido && alturaEhValida){
-        tdImc.textContent = calculaImc(peso,altura);
-    }    
+        imc = calculaImc(peso,altura);
+        tdImc.textContent = imc;
+        tdClassificacao.textContent = calculaClassificacao(imc);
+    }
 }
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
@@ -73,7 +76,8 @@ function obtemPacienteDoFormulario(form) {
         peso: form.peso.value,
         altura: form.altura.value,
         gordura: form.gordura.value,
-        imc: calculaImc(form.peso.value, form.altura.value)
+        imc: calculaImc(form.peso.value, form.altura.value),
+        classificacao: calculaClassificacao(calculaImc(form.peso.value, form.altura.value))
     }
 
     return paciente;
@@ -96,6 +100,7 @@ function montaTr(paciente) {
     pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
     pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
     pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+    pacienteTr.appendChild(montaTd(paciente.classificacao, "info-classificacao"));
 
     return pacienteTr;  
 }
@@ -145,4 +150,35 @@ function validaPaciente(paciente) {
     }
 
     return erros;
+}
+
+function calculaClassificacao(imc){
+    var classificacao;
+
+    if (imc < 16){
+        classificacao = "DESNUTRIDO";
+    } 
+    if (imc >= 16 && imc <= 17){
+        classificacao = "MAGREZA"
+    } 
+    if (imc > 17 && imc <= 18.5){
+        classificacao = "MAGRO LEVE"
+    } 
+    if (imc > 18.5 && imc <= 25){
+        classificacao = "SAUDÁVEL"
+    } 
+    if (imc > 25 && imc <= 30){
+        classificacao = "SOBREPESO"
+    } 
+    if (imc > 30 && imc <= 35){
+        classificacao = "OBESIDADE"
+    } 
+    if (imc > 35 && imc <= 40){
+        classificacao = "GORDÃO"
+    } 
+    if (imc > 40){
+        classificacao = "GORDALHAÇO"
+    }
+
+    return classificacao;
 }
